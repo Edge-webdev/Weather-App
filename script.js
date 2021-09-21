2459264a14bba3d155c5d2ed3b8289f1bcbb749f
EdgeO <73867028+Edge-webdev@users.noreply.github.com>
2021-09-21 16:53:11 -0600

    Changed API source

--- script.js
+++ script.js
@@ -2,48 +2,61 @@ const input = document.querySelector('input');
 const submit = document.querySelector('button');
 const forecastContainer = document.querySelector('.container');
 
+submit.addEventListener("click", fetchWeather)
+
 async function fetchWeather() {
-    let city = input.value[0].toUpperCase() + input.value.slice(1);
-    fetch(`http://api.weatherapi.com/v1/current.json?key=efde65d01e6643e8903112241211809&q=${city.toLowerCase()}&aqi=no`)
-        .then(response => response.json())
-        .then(data => forecastContainer.innerHTML = `
+    var city = input.value
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=815173988628159d2cf27189bb8b38b0&units=metric`);
+    let data = await res.json();
+
+    let { name } = data;
+    let { icon, description } = data.weather[0];
+    let { temp, humidity } = data.main;
+    let { speed } = data.wind;
+    let { visibility } = data;
+    let { rain } = data;
+
+    temp = Math.round(temp);
+    visibility = Math.round(visibility / 1000);
+
+    forecastContainer.innerHTML = `
     <div class="input-container">
         <input type="text" placeholder="Enter your city here" />
-        <button onclick="fetchWeather()">Search</button>
+        <button>Search</button>
     </div>
     <section class="weather-forecast">
-        <h1 class="title">The weather forecast in ${city} today is</h1>
+        <h1 class="title">The weather forecast in ${name} today is</h1>
         <img
-        src="https:${data.current.condition.icon}"
+        src="https://openweathermap.org/img/wn/${icon}@4x.png"
         alt="Weather Image"
         class="weather-img"
         />
-        <h3 class="weather-condition">${data.current.condition.text}</h3>
-        <h2 class="temperature">${data.current.temp_c}&deg;C</h2>
+        <h3 class="weather-condition">${description}</h3>
+        <h2 class="temperature">${temp}&deg;C</h2>
     </section>
     <section class="extra-info">
         <ul class="list">
         <li class="list-item">
             <div class="icon"><i class="fas fa-wind fa-3x"></i></div>
             <h1 class="list-item-title">Wind Speed</h1>
-            <h3 class="wind-speed">${data.current.wind_kph} km/h</h3>
+            <h3 class="wind-speed">${speed} km/h</h3>
         </li>
         <li class="list-item">
             <div class="icon"><i class="fas fa-tint fa-3x"></i></div>
             <h1 class="list-item-title">Humidity</h1>
-            <h3 class="wind-speed">${data.current.humidity}%</h3>
+            <h3 class="wind-speed">${humidity}%</h3>
         </li>
         <li class="list-item">
             <div class="icon"><i class="far fa-eye fa-3x"></i></div>
             <h1 class="list-item-title">Visibility</h1>
-            <h3 class="wind-speed">${data.current.vis_km} km</h3>
+            <h3 class="wind-speed">${visibility} km</h3>
         </li>
         <li class="list-item">
             <div class="icon"><i class="fas fa-cloud-rain fa-3x"></i></div>
             <h1 class="list-item-title">Precipitation</h1>
-            <h3 class="wind-speed">${data.current.precip_mm} mm</h3>
+            <h3 class="wind-speed">${rain ? rain["1h"] : 0.0} mm</h3>
         </li>
         </ul>
-    </section>`
-        )
+  </section>
+    `
 }
