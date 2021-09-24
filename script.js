693089a29ff58e364558a6d833c0f44c25eb0ce2
Edge <edgeo0314@gmail.com>
2021-09-23 20:08:11 -0600

    Removed clear button - Deleted clear button code - Added a class to the search button

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const input = document.querySelector('input');
 const submit = document.querySelector('button');
 const forecastContainer = document.querySelector('.container');
 
+<<<<<<< HEAD
 submit.addEventListener("click", fetchWeather)
+=======
+submit.addEventListener("click", fetchWeather);
+>>>>>>> 4339f11 (Removed clear button)
 
 async function fetchWeather() {
     var city = input.value
@@ -21,42 +25,44 @@ async function fetchWeather() {
 
     forecastContainer.innerHTML = `
     <div class="input-container">
-        <input type="text" placeholder="Enter your city here" />
-        <button>Search</button>
-    </div>
-    <section class="weather-forecast">
-        <h1 class="title">The weather forecast in ${name} today is</h1>
-        <img
-        src="https://openweathermap.org/img/wn/${icon}@4x.png"
-        alt="Weather Image"
-        class="weather-img"
-        />
-        <h3 class="weather-condition">${description}</h3>
-        <h2 class="temperature">${temp}&deg;C</h2>
+    <input type="text" placeholder="Enter your city here" />
+    <button class="search">Search</button>
+  </div>
+    <div class="weather-container">
+        <section class="weather-forecast">
+            <h1 class="title">The weather forecast in ${name} today is</h1>
+            <img
+            src="https://openweathermap.org/img/wn/${icon}@4x.png"
+            alt="Weather Image"
+            class="weather-img"
+            />
+            <h3 class="weather-condition">${description}</h3>
+            <h2 class="temperature">${temp}&deg;C</h2>
+        </section>
+        <section class="extra-info">
+            <ul class="list">
+            <li class="list-item">
+                <div class="icon"><i class="fas fa-wind fa-3x"></i></div>
+                <h1 class="list-item-title">Wind Speed</h1>
+                <h3 class="wind-speed">${speed} km/h</h3>
+            </li>
+            <li class="list-item">
+                <div class="icon"><i class="fas fa-tint fa-3x"></i></div>
+                <h1 class="list-item-title">Humidity</h1>
+                <h3 class="wind-speed">${humidity}%</h3>
+            </li>
+            <li class="list-item">
+                <div class="icon"><i class="far fa-eye fa-3x"></i></div>
+                <h1 class="list-item-title">Visibility</h1>
+                <h3 class="wind-speed">${visibility} km</h3>
+            </li>
+            <li class="list-item">
+                <div class="icon"><i class="fas fa-cloud-rain fa-3x"></i></div>
+                <h1 class="list-item-title">Precipitation</h1>
+                <h3 class="wind-speed">${rain ? rain["1h"] : 0.0} mm</h3>
+            </li>
+            </ul>
     </section>
-    <section class="extra-info">
-        <ul class="list">
-        <li class="list-item">
-            <div class="icon"><i class="fas fa-wind fa-3x"></i></div>
-            <h1 class="list-item-title">Wind Speed</h1>
-            <h3 class="wind-speed">${speed} km/h</h3>
-        </li>
-        <li class="list-item">
-            <div class="icon"><i class="fas fa-tint fa-3x"></i></div>
-            <h1 class="list-item-title">Humidity</h1>
-            <h3 class="wind-speed">${humidity}%</h3>
-        </li>
-        <li class="list-item">
-            <div class="icon"><i class="far fa-eye fa-3x"></i></div>
-            <h1 class="list-item-title">Visibility</h1>
-            <h3 class="wind-speed">${visibility} km</h3>
-        </li>
-        <li class="list-item">
-            <div class="icon"><i class="fas fa-cloud-rain fa-3x"></i></div>
-            <h1 class="list-item-title">Precipitation</h1>
-            <h3 class="wind-speed">${rain ? rain["1h"] : 0.0} mm</h3>
-        </li>
-        </ul>
-  </section>
+  </div>
     `
 }
