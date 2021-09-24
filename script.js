789470e4a6b6b8d0af795c0d9077e1d4eb84d68f
Edge <edgeo0314@gmail.com>
2021-09-23 21:00:54 -0600

    Fixed search bug - Instead of rendering the data all in the container, render the data seperately in different elements - This solves a bug where you can't search twice in a row

--- script.js
+++ script.js
@@ -1,68 +1,37 @@
 const input = document.querySelector('input');
-const submit = document.querySelector('button');
-const forecastContainer = document.querySelector('.container');
+const submit = document.querySelector('.search');
+const title = document.querySelector('.title');
+const weatherIcon = document.querySelector('.weather-img');
+const weatherCondition = document.querySelector('.weather-condition');
+const temperature = document.querySelector('.temperature');
+const windSpeed = document.querySelector('.wind-speed');
+const humidityDisplay = document.querySelector('.humidity');
+const visibilityDisplay = document.querySelector('.visibility');
+const precipitation = document.querySelector('.precipitation');
 
-<<<<<<< HEAD
-submit.addEventListener("click", fetchWeather)
-=======
 submit.addEventListener("click", fetchWeather);
->>>>>>> 4339f11 (Removed clear button)
 
 async function fetchWeather() {
-    var city = input.value
+    var city = input.value;
     let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=815173988628159d2cf27189bb8b38b0&units=metric`);
     let data = await res.json();
 
-    let { name } = data;
+    let { name, visibility, rain } = data;
     let { icon, description } = data.weather[0];
     let { temp, humidity } = data.main;
     let { speed } = data.wind;
-    let { visibility } = data;
-    let { rain } = data;
 
     temp = Math.round(temp);
     visibility = Math.round(visibility / 1000);
 
-    forecastContainer.innerHTML = `
-    <div class="input-container">
-    <input type="text" placeholder="Enter your city here" />
-    <button class="search">Search</button>
-  </div>
-    <div class="weather-container">
-        <section class="weather-forecast">
-            <h1 class="title">The weather forecast in ${name} today is</h1>
-            <img
-            src="https://openweathermap.org/img/wn/${icon}@4x.png"
-            alt="Weather Image"
-            class="weather-img"
-            />
-            <h3 class="weather-condition">${description}</h3>
-            <h2 class="temperature">${temp}&deg;C</h2>
-        </section>
-        <section class="extra-info">
-            <ul class="list">
-            <li class="list-item">
-                <div class="icon"><i class="fas fa-wind fa-3x"></i></div>
-                <h1 class="list-item-title">Wind Speed</h1>
-                <h3 class="wind-speed">${speed} km/h</h3>
-            </li>
-            <li class="list-item">
-                <div class="icon"><i class="fas fa-tint fa-3x"></i></div>
-                <h1 class="list-item-title">Humidity</h1>
-                <h3 class="wind-speed">${humidity}%</h3>
-            </li>
-            <li class="list-item">
-                <div class="icon"><i class="far fa-eye fa-3x"></i></div>
-                <h1 class="list-item-title">Visibility</h1>
-                <h3 class="wind-speed">${visibility} km</h3>
-            </li>
-            <li class="list-item">
-                <div class="icon"><i class="fas fa-cloud-rain fa-3x"></i></div>
-                <h1 class="list-item-title">Precipitation</h1>
-                <h3 class="wind-speed">${rain ? rain["1h"] : 0.0} mm</h3>
-            </li>
-            </ul>
-    </section>
-  </div>
-    `
+    input.value = '';
+
+    title.innerText = `The weather in ${name} today is`;
+    weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+    weatherCondition.innerText = `${description}`;
+    temperature.innerText = `${temp}°C`;
+    windSpeed.innerText = `${speed} km/h`;
+    humidityDisplay.innerText = `${humidity}%`;
+    visibilityDisplay.innerText = `${visibility} km`;
+    precipitation.innerText = `${rain ? rain["1h"] : 0.0} mm`
 }
